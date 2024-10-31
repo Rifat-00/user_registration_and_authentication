b679d218a2a4cead0cdfa1273e5b002ffb7ea35a
Rifat-00 <rtr.rifat22@gmail.com>
2024-10-31 16:00:24 +0600

    project scaffolding

---
+++ src/main/java/com/user/user_rgistration_and_authentication/UserRgistrationAndAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.user.user_rgistration_and_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRgistrationAndAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRgistrationAndAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/user/user_rgistration_and_authentication/UserRgistrationAndAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.user_rgistration_and_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRgistrationAndAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
